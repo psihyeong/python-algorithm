85873a6af6ae1dac79155a4f55808e16432dee5b
Park Si Hyeong <108376463+psihyeong@users.noreply.github.com>
2022-08-25 00:15:12 +0900

    220824 BFS

--- 220824_SWEA_10966.py
+++ 220824_SWEA_10966.py
@@ -3,15 +3,23 @@ from collections import deque
 TC = int(input())
 for tc in range(1,TC+1):
     N, M = map(int,input().split())
-    mapp = [list(input()) for _ in range(N)]
     result = [[-1 for _ in range(M)] for _ in range(N)]
     water = deque()
+#     mapp = [list(input()) for _ in range(N)]
+#     for i in range(N):
+#         for j in range(M):
+#             if mapp[i][j] == 'W':
+#                 water.append((i, j))
+#                 result[i][j] = 0
+
+# 메모리 초과 방지를 위해서 아래와 같은 코드로 대체.
     for i in range(N):
-        for j in range(M):
-            if mapp[i][j] == 'W':
+        row = input()
+        for j in range(len(row)):
+            if row[j] == 'W':
                 water.append((i, j))
                 result[i][j] = 0
-
+# 위 코드는 따로 지도 그래프를 만들지 않아도 됨. 메모리 감소
     result_val = 0
     # BFS
     while water:
@@ -22,7 +30,7 @@ for tc in range(1,TC+1):
          	# 범위를 벗어나지 않으면서
             if 0<=nx<M and 0<=ny<N:
             	if result[ny][nx] == -1:
-                    # 방문값이 0이면서, W가 아니면
+                    # 방문값이 -1이면서(W에서 0 부터 출발하지만, L을 확인하고 싶을 때)
                     water.append((ny,nx))
                     result[ny][nx] = result[wy][wx] + 1
         result_val += result[wy][wx]
