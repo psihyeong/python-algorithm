d55a913358f80ebb18eb99b5e20a3cc6295f6a58
Park Si Hyeong <108376463+psihyeong@users.noreply.github.com>
2022-08-31 00:22:05 +0900

    220830 BFS, 3차원배열

--- 220830_BOJ_7569.py
+++ 220830_BOJ_7569.py
@@ -1,41 +1,48 @@
 # BOJ 7569번 토마토
 
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 W,H,K = map(int,input().split())
-tomato = [[list(map(int,input().split())) for _ in range(H)] for _ in range(K)]
+tomato = []
+baby = 0
 # 큐에 최초 익은 토마토 좌표 담기
 queue = deque()
 for k in range(K):
+    tmp = []
     for i in range(H):
+        tmp.append(list(map(int,sys.stdin.readline().split())))
         for j in range(W):
-            if tomato[k][i][j] == 1:
+            if tmp[i][j]==1:
                 queue.append((k,i,j))
-# 3차원 BFS                
+            # baby는 안 익은 토마토 개수
+            elif tmp[i][j]==0:
+                baby += 1
+    tomato.append(tmp)
+    
+# 3차원 BFS
+grown = 0
+result = 1
 while queue:
     s,y,x = queue.popleft()
     for ds,dy,dx in [(1,0,0),(-1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)]:
         ns = s+ds
         ny = y+dy
         nx = x+dx
-        if 0<=ns<K and 0<=ny<H and 0<=nx<W:
-            if tomato[ns][ny][nx] == 0:
-                queue.append((ns,ny,nx))
-                # 거리 = 익는 데 걸리는 최소 일 수
-                tomato[ns][ny][nx] = tomato[s][y][x] + 1
-# 출력                
-result = 0
-all_cooked = 1
-for k in range(K):
-    for i in range(H):
-        for j in range(W):
-            if tomato[k][i][j] == 0:
-                all_cooked = 0
-            else:
-                if tomato[k][i][j] > result:
-                    result = tomato[k][i][j]
-                    
-if all_cooked:
+        if 0<=ns<K and 0<=ny<H and 0<=nx<W and tomato[ns][ny][nx] == 0:
+            queue.append((ns,ny,nx))
+            # 익은 토마토 개수
+            grown += 1
+            # 거리 = 익는 데 걸리는 최소 일 수
+            tomato[ns][ny][nx] = tomato[s][y][x] + 1
+            # result 갱신
+            if tomato[ns][ny][nx] > result:
+                result = tomato[ns][ny][nx]
+                
+# 안 익은 토마토의 개수와 익은 토마토의 개수가 같다면 다 익은 것이므로 result 출력                                   
+if baby == grown:
     print(result-1)
+# 안 익은게 존재하면 -1
 else:
     print(-1)
