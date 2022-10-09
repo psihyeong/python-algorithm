373cdc380a5da486335c224aae16cde53e4def6a
qkrtlgud3286 <qkrtlgud3286@naver.com>
2022-10-09 19:45:55 +0900

    221009 브루트포스

---
+++ 221009_BOJ_14501.py
@@ -0,0 +1,17 @@
+# BOJ 14501번 퇴사
+def dfs(startwith,val):
+    global result
+    if result < val:
+        result = val
+    for i in range(startwith,N+1):
+        new_val = val + schedule[i][1]
+        new_start = i + schedule[i][0]
+        if new_start <= N+1:
+            dfs(new_start, new_val)
+
+
+N = int(input())
+schedule = [[0,0]] + [list(map(int,input().split())) for _ in range(N)]
+result = 0
+dfs(1,0)
+print(result)
