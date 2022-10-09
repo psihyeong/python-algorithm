5efa9d0c4a377da28cba1c9b95dc6b01541a993a
qkrtlgud3286 <qkrtlgud3286@naver.com>
2022-10-09 21:45:49 +0900

    221009 브루트포스, DP

--- 221009_BOJ_14501.py
+++ 221009_BOJ_14501.py
@@ -1,6 +1,7 @@
 # BOJ 14501번 퇴사
-# 브루트포스
+# 브루트포스, 다이나믹 프로그래밍
 
+# 브루트포스 풀이
 def dfs(startwith,val):
     global result
     if result < val:
@@ -11,9 +12,20 @@ def dfs(startwith,val):
         if new_start <= N+1:
             dfs(new_start, new_val)
 
-
 N = int(input())
 schedule = [[0,0]] + [list(map(int,input().split())) for _ in range(N)]
 result = 0
 dfs(1,0)
 print(result)
+
+
+# DP 풀이
+N = int(input())
+schedule =[[0,0]] + [list(map(int,input().split())) for _ in range(N)]
+# 인덱스 날짜에 상담이 끝나고 받는 최대 금액
+dp = [0 for _ in range(21)]
+for i in range(1,N+1):
+    length = schedule[i][0] - 1
+    dp[i] = max(dp[i-1],dp[i])
+    dp[i+length] = max(dp[i-1] + schedule[i][1], dp[i+length])
+print(dp[N])
