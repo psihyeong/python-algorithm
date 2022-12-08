1a9ffdf657ade285f5977663f96b3a8461b4aa0a
qkrtlgud3286 <qkrtlgud3286@naver.com>
2022-12-08 23:28:04 +0900

    221208 이분 탐색

---
+++ 221208_BOJ_1654.py
@@ -0,0 +1,29 @@
+# BOJ 1654번 랜선 자르기
+
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+arr = [int(sys.stdin.readline()) for _ in range(N)]
+result = 0
+
+# 랜선의 길이가 될 수 있는 모든 경우를 이분탐색
+l, r = 1, max(arr)
+while l <= r:
+    # mid 길이로
+    mid = (l + r) // 2
+    line_cnt = 0
+
+    for num in arr:
+        # 만들 수 있는 랜선의 개수가
+        line_cnt += num//mid
+
+    # K개 보다 크거나 같으면
+    if line_cnt >= K:
+        result = max(result, mid)
+        # 더 큰 길이를 봐야지
+        l = mid + 1
+    else:
+        # 더 작은 길이를 봐야지
+        r = mid - 1
+
+print(result)
