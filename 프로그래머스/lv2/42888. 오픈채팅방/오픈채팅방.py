0ffb39659f8ea455ef984517f1be00e36b284da7
Park Si Hyeong <108376463+psihyeong@users.noreply.github.com>
2023-03-07 00:10:24 +0900

    [level 2] Title: 오픈채팅방, Time: 110.72 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,27 +1,19 @@
 def solution(record):
-    record_lst = []
+    name_dict = {}
+    log = []
+    
     for rec in record:
-        record_lst.append(list(map(str,rec.split())))
-        
-    
-    nickname_hash = {}
-    answer = []
-    
-    for i in record_lst:
+        i = list(map(str,rec.split()))
         if len(i) == 3 and i[0] == "Enter":
-            answer.append((i[1],"Enter"))
-            nickname_hash[i[1]] = i[2]
+            name_dict[i[1]] = i[2]
+            log.append((i[1], "님이 들어왔습니다."))
         elif len(i) == 2 and i[0] == "Leave":
-            answer.append((i[1],"Leave"))
+            log.append((i[1], "님이 나갔습니다."))
         else:
-            nickname_hash[i[1]] = i[2]
-            
-    ans = []
-    
-    for user_id, status in answer:
-        if status == "Enter":
-            ans.append(f"{nickname_hash[user_id]}님이 들어왔습니다.")
-        else:
-            ans.append(f"{nickname_hash[user_id]}님이 나갔습니다.")
-
-    return ans
+            name_dict[i[1]] = i[2]
+                          
+    answer = []
+    for user_id, status in log:
+        answer.append(name_dict[user_id]+status)
+        
+    return answer
