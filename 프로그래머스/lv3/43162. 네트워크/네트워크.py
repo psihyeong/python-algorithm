46ae3abe31f1921e6b25e8e296186ece9bd5b13c
Park Si Hyeong <108376463+psihyeong@users.noreply.github.com>
2023-03-20 16:28:36 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,26 +1,25 @@
 from collections import deque
+def bfs(num,visited,computers):
+    q = deque()
+    q.append(num)
+    visited[num] = 1
 
+    while q:
+        node = q.popleft()
+
+        for idx, is_connect in enumerate(computers[node]):
+            if is_connect and idx != node and not visited[idx]:
+                q.append(idx)
+                visited[idx] = 1
+                    
 def solution(n, computers):
     answer = 0
     
     visited = [0 for _ in range(200)]
-    
-    def bfs(num):
-        q = deque()
-        q.append(num)
-        visited[num] = 1
-        
-        while q:
-            node = q.popleft()
-            
-            for idx, is_connect in enumerate(computers[node]):
-                if is_connect and idx != node and not visited[idx]:
-                    q.append(idx)
-                    visited[idx] = 1
-            
+       
     for i in range(len(computers)):
         if not visited[i]:
-            bfs(i)
+            bfs(i,visited,computers)
             answer += 1
         
     return answer
